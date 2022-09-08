81705403167af5e178bb30f3a407eae73e20a9e1
jobdelac <alanjosepbdlc@ciencias.unam.mx>
2022-09-08 14:19:28 -0500

    adding exec_128

--- exec_128/ReverseLookup/src/RevLookup.scala
+++ exec_128/ReverseLookup/src/RevLookup.scala
@@ -8,20 +8,21 @@ object RevLookup {
       "Monkey" -> 0, "Rooster" -> 1, "Dog" -> 2, "Pig" -> 3, "Rat" -> 4, "Ox" -> 5,
       "Tiger" -> 6, "Hare" -> 7, "Xolotl" -> 2, "Cacomistle" -> 2, "Ocelot" -> 11, "Cenzontle" -> 0)
 
+    println(map_zodiac)
     println("Type a number between 0 and 11 to get animal keys from Chinese-Mexa Calendar: ")
     val cal_value = readInt()
+    val key_animal = reverseLookup(map_zodiac,cal_value)
 
-    val key_animal = reverseLookup(map_zodiac,key_animal)
-
-    println(s"The animals from calendar which match $cal_value are: ")
-    key_animal.foreach(animal)
-
-
+    if(key_animal.isEmpty){
+      println(s"ERROR: There are no animals which match value $cal_value")
+    }else{
+      println(s"The animals from calendar which match $cal_value are: ")
+      for (animal <- key_animal) print(s"$animal ")
+    }
   }
+
   def reverseLookup(arg: Map[String, Int], value: Int): List[String] ={
-
     var arg_keys = arg.filter(_._2==value).keys
-
     return arg_keys.toList
   }
 }
