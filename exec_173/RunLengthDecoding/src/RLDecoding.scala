000cd51551c43ea5248fe8391d5252cc4cf41aab
jobdelac <alanjosepbdlc@ciencias.unam.mx>
2022-09-09 01:46:42 -0500

    adding last exercise, exec 173

--- exec_173/RunLengthDecoding/src/RLDecoding.scala
+++ exec_173/RunLengthDecoding/src/RLDecoding.scala
@@ -6,9 +6,15 @@ object RLDecoding {
 
     val encodeList: ListBuffer[(String,Int)] = ListBuffer(("A",12),("B",4),("A",6),
       ("B",1), ("Z",6))
-  }
-  def decoding(arg: ListBuffer[(String,Int)]): String = {
-    
+    val stringOut = decoding(encodeList,0)
 
+    println(stringOut)
+  }
+  def decoding(arg: ListBuffer[(String,Int)], i: Int):String ={
+    if(i < arg.length){
+      return decoding(arg,i+1) + (arg(i)._1 * arg(i)._2)
+    }else{
+      return ""
+    }
   }
 }
